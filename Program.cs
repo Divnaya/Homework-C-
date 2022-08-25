f9d40a453ded6c7faac2b4697ef49c2f8af1059b
Olya <alexadivnaya@rambler.ru>
2022-08-25 20:05:21 +0300

    Задачи

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿// Задача 41.
-//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿//Задача 41.
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+//0, 7, 8, -2, -2 -> 2; 1, -7, 567, 89, 223-> 3
 
 Console.Clear();
 int CountGreaterZero(int[] a)
@@ -16,21 +17,23 @@ int[] CreateArray(int num)
     int[] array = new int[num];
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"Input {i + 1} number: ");
+        Console.Write($"Введите {i + 1} число: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
     }
     return array;
 }
-Console.Write("Input count numbers: ");
+Console.Write("Введите количество чисел: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Count numbers greater than zero = {CountGreaterZero(CreateArray(n))}");
+Console.WriteLine($"Количество чисел после нуля = {CountGreaterZero(CreateArray(n))}");
 
 
 
+//__________________________________________________________________________________________________________________________________________________
 
 
-/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
-//значения b1, k1, b2 и k2 задаются пользователем.
+//Задача 43.
+//Напишите программу, которая найдёт точку пересечения двух прямых,заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем. b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 Console.Clear();
 double[] StraightA(double b1, double k1)
@@ -81,4 +84,3 @@ Console.Write("Введите b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 ShowResult(IntersectionPoint(StraightA(k1, b1), StraightB(k2, b2)));
 Console.WriteLine();
-*/
